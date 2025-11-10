000b43c8a6620e2ee145e51ef0a56846e44873d9
Johan192004 <johanruma@gmail.com>
2025-11-09 23:17:22 -0500

    feat: implement initial spring structure

---
+++ src/main/java/com/johan/db_cloud/DbCloudApplication.java
@@ -0,0 +1,13 @@
+package com.johan.db_cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbCloudApplication.class, args);
+	}
+
+}
